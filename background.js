c294f6179d4e0cefc47c288e709bccbaa2d722d9
NightsoulCC4 <StickyCoffee8360@gmail.com>
2021-08-23 20:29:02 +0700

    changed algorithm

--- background.js
+++ background.js
@@ -1,16 +1,26 @@
+const chromePswd = "chrome://settings/passwords";
+const settingPage = "chrome://settings/";
+
 chrome.tabs.onActivated.addListener((tab) => {
   chrome.tabs.get(tab.tabId, (current_tab_info) => {
     if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
-      chrome.tabs.insertCSS(null, { file: "../style/app.css" });
+      chrome.tabs.insertCSS(null, { file: "./mystyles.css" });
       chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
-        console.log("i injected")
+        console.log("i injected", tab.tabId, "+", current_tab_info.url)
       );
     }
-    if (/^edge:\/\/settings/) {
+
+    //ทำงานเมื่อเข้าหน้า settings
+    if (current_tab_info.url.slice(0, 18) == settingPage) {
       console.log("Someone access to the settings page");
-        if (/^edge:\/\/settings/.test(current_tab_info.url)) {
-          console.log("Lunchtime attacks!!");
-        }
+
+      //ฟังก์ชันนี้จะทำงานเมื่อมีคนเข้าหน้า password
+      if (chromePswd == current_tab_info.url) {
+        console.log("God morgon");
+        chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
+          console.log("coming to maskedId.js")
+        );
+      }
     }
   });
 });
--- foreground.js
+++ foreground.js
@@ -1,2 +1,23 @@
 document.querySelector(".lnXdpd").classList.add("spinspinspin");
-document.querySelector(".c01466").append("Exited");
+
+// const first = document.createElement('button');
+// first.innerText = "SET DATA";
+// first.id = "first";
+
+// const second = document.createElement('button');
+// second.innerText = "SHOUTOUT TO BACKEND";
+// second.id = "second";
+
+// document.querySelector('body').appendChild(first);
+// document.querySelector('body').appendChild(second);
+
+// first.addEventListener('click', () => {
+//     chrome.storage.local.storage({"password": "123"});
+//     console.log("set data");
+// });
+
+console.log("from background, I injected");
+
+//id ต่างกันทุกเครื่องหรือไม่
+//Physical ATTACK เวลาน้อย
+//
