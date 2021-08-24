d9b773f6211145b8047156e380b1b7c70d077f31
Norrathep <nrattana@uci.edu>
2021-08-24 17:32:39 +0700

    can inspect password's link on Edge

--- background.js
+++ background.js
@@ -5,28 +5,27 @@ chrome.tabs.getCurrentTab
 
 chrome.tabs.onActivated.addListener((tab) => {
   chrome.tabs.get(tab.tabId, (current_tab_info) => {
-    if (/^https:\/\/www\.google/.test(current_tab_info.url)) {
-      chrome.tabs.insertCSS(null, { file: "style/mystyles.css" });
-      chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
-        console.log("i injected", tab.tabId, "+", current_tab_info.url)
-      );
-    }
 
     //ทำงานเมื่อเข้าหน้า settings
-    if (current_tab_info.url.slice(0, 18) == settingPage) {
+    /*if (current_tab_info.url.slice(0, 18) == settingPage) {
       console.log("Someone access to the settings page");
       console.log(tab);
 
       //ฟังก์ชันนี้จะทำงานเมื่อมีคนเข้าหน้า password
       if (chromePswd == current_tab_info.url) {
-      //   chrome.tabs.executeScript(null, { file: "./maskedImage.js" }, () =>
-      //     console.log("MaskedImaged", tab.tabId, "+", current_tab_info.url)
-      // );
-      alert("these are passwords");
-      chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
-      console.log("I'm going to make my script work", tab.tabId, "+", current_tab_info.url)
-    );
+        //   chrome.tabs.executeScript(null, { file: "./maskedImage.js" }, () =>
+        //     console.log("MaskedImaged", tab.tabId, "+", current_tab_info.url)
+        // );
+        alert("these are passwords");
+        chrome.tabs.executeScript(null, { file: "./foreground.js" }, () =>
+          console.log("I'm going to make my script work", tab.tabId, "+", current_tab_info.url)
+        );
       }
-    }
+    }*/
+
+    chrome.tabs.executeScript(null, {file: "./foreground.js"}, () => console.log("bra bra bra") );
+
+
+
   });
 });
--- foreground.js
+++ foreground.js
@@ -1,7 +1,29 @@
-document.querySelector(".lnXdpd").classList.add("spinspinspin");
+//document.querySelector(".lnXdpd").classList.add("spinspinspin");
 
-let mic = document.getElementsByClassName('goxjub');
-console.log(mic)
+//let mic = document.getElementsByClassName('goxjub');
+//console.log(mic)
+
+// Important: enable the following flag: edge://flags/#extensions-on-edge-urls
+if (typeof oak === 'undefined') {
+    //var oak = document.getElementById("company");
+    var oak = document.getElementById("passwordShowPasswordButton_0");
+    oak = oak.parentElement.parentElement.childNodes;
+    //oak = element.shadowRoot;
+    console.log("create new oak var");
+} else {
+    //oak = document.getElementById("company");
+    oak = document.getElementById("passwordShowPasswordButton_0");
+    oak = oak.parentElement.parentElement.childNodes;
+    //oak = element.shadowRoot;
+    console.log("reuse oak var");
+}
+
+for (var i = 0; i < oak.length; i++) {
+    if(oak[i].getAttribute("data-cellid") == "uniqueKey") {
+        var loginLink = oak[i].childNodes[0].getAttribute("href");
+        console.log(i+" "+loginLink);
+    }
+}
 
 // const first = document.createElement('button');
 // first.innerText = "SET DATA";
