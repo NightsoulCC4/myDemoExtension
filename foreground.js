e5348e5ffeb43329fcc21707fc2b72bf72da05c3
NightsoulCC4 <StickyCoffee8360@gmail.com>
2021-09-22 15:32:19 +0700

    detectable on each id

--- foreground.js
+++ foreground.js
@@ -3,51 +3,62 @@
 var pswdBtnCounter = 0;
 var pswdBtns = [];
 
-console.log("Hello foreground");
 console.log("Password Counter is ", pswdBtnCounter);
 
-var a = document.getElementById('passwordShowPasswordButton_0');
-console.log(a.id);
+// pswdBtns[pswdBtnCounter] = document.getElementById("passwordShowPasswordButton_139");
+// console.log("You're", pswdBtns[pswdBtnCounter]);
 
-while(pswdBtnCounter <= pswdBtns.length){
-    var pswdBtnClick = document.getElementById('passwordShowPasswordButton_', pswdBtnCounter);
-    pswdBtnClick.addEventListener('click', function TestClick () {
-        alert("Ni Hao Zhong Gao Ren!!", pswdBtnCounter);
-    });
-    pswdBtns[pswdBtnCounter] = document.getElementById('passwordShowPasswordButton_', pswdBtnCounter).id;
-    console.log(pswdBtns[pswdBtnCounter]);
-    pswdBtnCounter++;
+console.log("Password counter = ", pswdBtnCounter);
+console.log("Password button list = ", pswdBtns);
+
+for(i = 0; i<=pswdBtns.length; i++){
+    if(document.getElementById("passwordShowPasswordButton_"+ i) == null){
+        console.log("This is your limit!!");
+       i++; 
+    }
+    else{
+        pswdBtns[i] = document.getElementById("passwordShowPasswordButton_"+ i);
+        console.log(pswdBtns[i].id);
+        console.log(i);
+    }
 }
 
-console.log(pswdBtnCounter);
 
-
-
-
-
-
-
-// if (typeof oak === 'undefined') {
-//     var oak = document.getElementById("passwordShowPasswordButton_0");
-//     oak = oak.parentElement.parentElement.childNodes;
-//     // oak = element.shadowRoot;
-//     console.log(oak)
-//     console.log("create new oak var");
-// } 
-// else {
-//     oak = document.getElementById("passwordShowPasswordButton_0");
-//     oak = oak.parentElement.parentElement.childNodes;
-//     // oak = element.shadowRoot;
-//     console.log("reuse oak var");
+// while(pswdBtnCounter <= pswdBtns.length){
+//     var pswdBtnClick = document.getElementById('passwordShowPasswordButton_', pswdBtnCounter);
+//     pswdBtnClick.addEventListener('click', function TestClick () {
+//         alert("Ni Hao Zhong Gao Ren!!", pswdBtnCounter);
+//     });
+//     pswdBtns[pswdBtnCounter] = document.getElementById('passwordShowPasswordButton_', pswdBtnCounter).id;
+//     console.log(pswdBtns[pswdBtnCounter]);
+//     pswdBtnCounter++;
 // }
 
-// for (var i = 0; i < oak.length; i++) {
-//     if(oak[i].getAttribute("data-cellid") == "uniqueKey") {
-//         var loginLink = oak[i].childNodes[0].getAttribute("href");
-//         console.log(i+" "+loginLink);
-//     }
-// }
 
-//id ต่างกันทุกเครื่องหรือไม่
-//Physical ATTACK เวลาน้อย
-//
+
+if (typeof oak === 'undefined') {
+    var oak = document.getElementById("passwordShowPasswordButton_0");
+    oak = oak.parentElement.parentElement.childNodes;
+    // oak = element.shadowRoot;
+    console.log(oak)
+    console.log("create new oak var");
+} 
+else {
+    oak = document.getElementById("passwordShowPasswordButton_0");
+    oak = oak.parentElement.parentElement.childNodes;
+    // oak = element.shadowRoot;
+    console.log("reuse oak var");
+}
+
+for (var i = 0; i < oak.length; i++) {
+    if(oak[i].getAttribute("data-cellid") == "uniqueKey") {
+        var loginLink = oak[i].childNodes[0].getAttribute("href");
+        console.log(i+" "+loginLink);
+        console.log(oak.length);
+    }
+}
+
+// var pswdBtnClick = document.getElementById('passwordShowPasswordButton_0');
+//     pswdBtnClick.addEventListener('click', function TestClick () {
+//         alert("Ni Hao Zhong Gao Ren!!");
+//     });
